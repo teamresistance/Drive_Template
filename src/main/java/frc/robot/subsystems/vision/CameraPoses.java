48a24d28b06b469c298ea9d0ae72c8af53753a1c
darkpotatoo <darkpotatoben@gmail.com>
2025-09-07 15:37:35 -0400

    updated camera poses

--- src/main/java/frc/robot/subsystems/vision/CameraPoses.java
+++ src/main/java/frc/robot/subsystems/vision/CameraPoses.java
@@ -5,48 +5,40 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
-/** Configuration class containing the poses of all cameras on the robot for vision processing. */
 public class CameraPoses {
+    public static final Pose3d[] cameraPoses =
+        new Pose3d[] {
+            // Front Left
+            new Pose3d(
+                new Translation3d(0.256, 0.213, 0.267), // Right camera translation (X, Y, Z)
+                new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(-15))),
+            // Front Right
+            new Pose3d(
+                new Translation3d(0.268, -0.213, 0.267), // Left camera translation (X, Y, Z)
+                new Rotation3d(
+                    0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45))), // in radians btw
+            //        // Back Right
+            new Pose3d(
+                new Translation3d(-0.235, -0.235, 0.267), // Left camera translation (X, Y, Z)
+                new Rotation3d(
+                    0.0,
+                    Units.degreesToRadians(-12.63),
+                    Units.degreesToRadians(-45 - 90))), // in radians btw
 
-  private CameraPoses() {
-    throw new IllegalStateException("Utility class");
-  }
+            // front_center
+            new Pose3d(
+                new Translation3d(
+                    Units.inchesToMeters(12.5 - 0.9375),
+                    Units.inchesToMeters(3.0),
+                    Units.inchesToMeters(6.5)), // Left camera translation (X, Y, Z)
+                new Rotation3d(
+                    Units.degreesToRadians(0.0),
+                    Units.degreesToRadians(-23.0),
+                    Units.degreesToRadians(-1.0))), // in radians btw
 
-  public static final Pose3d[] cameraPoses =
-      new Pose3d[] {
-        // Front Left
-        new Pose3d(
-            new Translation3d(0.235, 0.235, 0.267), // Right camera translation (X, Y, Z)
-            new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45))),
-        // Front Right
-        new Pose3d(
-            new Translation3d(0.235, -0.235, 0.267), // Left camera translation (X, Y, Z)
-            new Rotation3d(
-                0.0,
-                Units.degreesToRadians(-12.63),
-                Units.degreesToRadians(-45))), // in radians btw
-        //        // Back Right
-        new Pose3d(
-            new Translation3d(-0.235, -0.235, 0.267), // Left camera translation (X, Y, Z)
-            new Rotation3d(
-                0.0,
-                Units.degreesToRadians(-12.63),
-                Units.degreesToRadians(-45.0 - 90))), // in radians btw
-
-        // front_center
-        new Pose3d(
-            new Translation3d(
-                Units.inchesToMeters(12.5 - 0.9375),
-                Units.inchesToMeters(3.0),
-                Units.inchesToMeters(6.5)), // Left camera translation (X, Y, Z)
-            new Rotation3d(
-                Units.degreesToRadians(0.0),
-                Units.degreesToRadians(-23.0),
-                Units.degreesToRadians(-1.0))), // in radians btw
-
-        //         Back Left
-        new Pose3d(
-            new Translation3d(-0.235, 0.235, 0.267), // Right camera translation (X, Y, Z)
-            new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45.0 + 90))),
-      };
+            //         Back Left
+            new Pose3d(
+                new Translation3d(-0.235, 0.235, 0.267), // Right camera translation (X, Y, Z)
+                new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45 + 90))),
+        };
 }
