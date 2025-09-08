4c434a851820f70c968f4e7a9d2cd0341a2b03f1
darkpotatoo <darkpotatoben@gmail.com>
2025-09-08 18:52:39 -0400

    double stuff

--- src/main/java/frc/robot/subsystems/vision/CameraPoses.java
+++ src/main/java/frc/robot/subsystems/vision/CameraPoses.java
@@ -11,19 +11,19 @@ public class CameraPoses {
         // Front Left
         new Pose3d(
             new Translation3d(0.256, 0.213, 0.267), // Right camera translation (X, Y, Z)
-            new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(-15))),
+            new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(-15.0))),
         // Front Right
         new Pose3d(
             new Translation3d(0.268, -0.213, 0.267), // Left camera translation (X, Y, Z)
             new Rotation3d(
-                0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45))), // in radians btw
+                0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45.0))), // in radians btw
         //        // Back Right
         new Pose3d(
             new Translation3d(-0.235, -0.235, 0.267), // Left camera translation (X, Y, Z)
             new Rotation3d(
                 0.0,
                 Units.degreesToRadians(-12.63),
-                Units.degreesToRadians(-45 - 90))), // in radians btw
+                Units.degreesToRadians(-45.0 - 90.0))), // in radians btw
 
         // front_center
         new Pose3d(
@@ -39,6 +39,6 @@ public class CameraPoses {
         //         Back Left
         new Pose3d(
             new Translation3d(-0.235, 0.235, 0.267), // Right camera translation (X, Y, Z)
-            new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45 + 90))),
+            new Rotation3d(0.0, Units.degreesToRadians(-12.63), Units.degreesToRadians(45.0 + 90.0))),
       };
 }
