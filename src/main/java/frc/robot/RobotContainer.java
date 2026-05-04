342c01cb7780597cdfb4212e1bbc249dae8e5019
Bavithran Chandrahasan <132094588+cwbavi@users.noreply.github.com>
2026-05-04 04:55:15 -0400

    Fix typo in JavaDoc for getAutonomousCommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -211,7 +211,7 @@ public class RobotContainer {
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
-   * @return the command to run in autonoSmous
+   * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
     Command autoCommand = autoChooser.get();
